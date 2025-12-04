469b43199530263ab95809ae8491ff21b1b7f1f9
Avr1lls <saraleguizamon1234@gmail.com>
2025-12-04 14:04:30 -0500

    Comments (NotificadorPush)

--- ejercicios/src/main/java/ejercicio5/NotificadorPush.java
+++ ejercicios/src/main/java/ejercicio5/NotificadorPush.java
@@ -1,8 +1,8 @@
 package ejercicio5;
 
-public class NotificadorPush implements INotificador {
+public class NotificadorPush implements INotificador { // Implementing the INotificador interface
     @Override
-    public void enviar(String mensaje, String destinatorio) {
+    public void enviar(String mensaje, String destinatorio) { // Overriding the "enviar" method
         System.out.println("Enviando notificación push a " +  destinatorio + "... el mensaje es: " + mensaje);
     }
     
