11e973826cbcc7a0ec86f84cb610fd44eabe228b
Avr1lls <saraleguizamon1234@gmail.com>
2025-12-04 14:05:14 -0500

    Comments (GestorDeAlertas)

--- ejercicios/src/main/java/ejercicio5/GestorDeAlertas.java
+++ ejercicios/src/main/java/ejercicio5/GestorDeAlertas.java
@@ -2,13 +2,13 @@ package ejercicio5;
 import java.util.List;
 
 public class GestorDeAlertas {
-    private List<INotificador> notificadores;
+    private List<INotificador> notificadores; // List to hold different notifiers
 
-    public GestorDeAlertas(List<INotificador> notificadores) {
+    public GestorDeAlertas(List<INotificador> notificadores) { // Constructor to initialize the notifiers list
         this.notificadores = notificadores;
 
     }
-    public void dispararAlertaMaxima(String mensaje, String destinatario) {
+    public void dispararAlertaMaxima(String mensaje, String destinatario) { // Method to send alert using all notifiers
         for (int i = 0; i < notificadores.size(); i++) {
             INotificador notificador = notificadores.get(i);
             notificador.enviar(mensaje, destinatario);
