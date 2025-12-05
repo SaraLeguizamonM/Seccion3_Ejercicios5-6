88e0727f2a2523dd81801bf03e7f3bde6a87a627
Avr1lls <saraleguizamon1234@gmail.com>
2025-12-05 11:56:21 -0500

    Write comments in Irepositorio (interface)

--- ejercicios/src/main/java/ejercicio6/IRepositorio.java
+++ ejercicios/src/main/java/ejercicio6/IRepositorio.java
@@ -1,8 +1,7 @@
 package ejercicio6;
 
 public interface IRepositorio {
-    public void guardar(Object data);
-    public void leer(String id);
-    public void eliminar(String id);
-    
+    public void guardar(Object data); // Method to save data
+    public void leer(String id); // Method to read data by id
+    public void eliminar(String id); // Method to delete data by id
 }
