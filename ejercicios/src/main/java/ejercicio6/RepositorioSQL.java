82f1d78b8908e653979da0a08d0768807c62c82b
Avr1lls <saraleguizamon1234@gmail.com>
2025-12-05 09:29:57 -0500

    add other methods in RepositorioSQL

--- ejercicios/src/main/java/ejercicio6/RepositorioSQL.java
+++ ejercicios/src/main/java/ejercicio6/RepositorioSQL.java
@@ -8,7 +8,8 @@ public class RepositorioSQL implements IRepositorio {
         for(int i = 0; i < guardar.length; i++){
             if (guardar[i] == null) {
                 guardar[i] = data.toString();
-                System.out.println(" INSERT INTO tabla (id, data) VALUES ('" + i + "', '" + data + "')");
+                System.out.println("SQL> INSERT INTO tabla (id, data) VALUES ('" + i + "', '" + data + "')");
+                break;
             }else if (i == 9 && !(guardar[i] == null)) {
                 System.out.println("SQL Error [1205]: Error: no se puede insertar porque la tabla alcanzó su capacidad máxima." );
                 break;
@@ -18,12 +19,15 @@ public class RepositorioSQL implements IRepositorio {
     }
     @Override
     public void leer(String id) {
-
+        System.out.println("SQL> SELECT * FROM tabla WHERE id = '" + id + "';");
+        System.out.println(guardar[Integer.parseInt(id)]);
 
     }
     @Override
     public void eliminar(String id) {
-
+        System.out.println("SQL> DELETE FROM tabla WHERE id = '" + id + "';");
+        guardar[Integer.parseInt(id)] = null;
+        System.out.println("1 row deleted.");
     }
 }
 
