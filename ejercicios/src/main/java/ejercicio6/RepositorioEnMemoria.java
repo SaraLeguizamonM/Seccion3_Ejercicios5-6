f36ea97c512b938fef536a56bdf47435e65f71c7
Avr1lls <saraleguizamon1234@gmail.com>
2025-12-05 09:31:04 -0500

    add other methods in RepositorioEnMemoria

--- ejercicios/src/main/java/ejercicio6/RepositorioEnMemoria.java
+++ ejercicios/src/main/java/ejercicio6/RepositorioEnMemoria.java
@@ -8,22 +8,26 @@ public class RepositorioEnMemoria implements IRepositorio {
         for(int i = 0; i < guardar.length; i++){
             if (guardar[i] == null) {
                 guardar[i] = data.toString();
-                System.out.println("Se ha guardado en la posicion: " + i);
+                System.out.println("MEM> Se ha guardado en la posicion: " + i);
+                break;
             }else if (i == 9 && !(guardar[i] == null)) {
-                System.out.println("Error: Memoria llena" );
+                System.out.println("MEM> Error: Memoria llena" );
                 break;
             }
         }
-
     }
 
     @Override
     public void leer(String id) {
-
+        System.out.println("MEM> Leyendo posicion... " + id );
+        System.out.println(guardar[Integer.parseInt(id)]);
     }
 
     @Override
     public void eliminar(String id) {
+        System.out.println("MEM> Eliminando posicion... " + id );
+        guardar[Integer.parseInt(id)] = null;
+        System.out.println("Eliminado.");
 
     }
 }
